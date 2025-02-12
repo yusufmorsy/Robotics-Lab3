86d702c601b9c541a54fec4369744ec20adeae1a
yusuf morsy <143475466+yusufmorsy@users.noreply.github.com>
2025-02-12 15:55:54 -0700

    Init Files

---
+++ lab3.py
@@ -0,0 +1,75 @@
+"""csci3302_lab2 controller."""
+
+# You may need to import some classes of the controller module.
+import math
+from controller import Robot, Motor, DistanceSensor, Supervisor
+import numpy as np
+
+pose_x = 0
+pose_y = 0
+pose_theta = 0
+
+# create the Robot instance.
+robot = Supervisor()
+
+# ePuck Constants
+EPUCK_AXLE_DIAMETER = 0.053 # ePuck's wheels are 53mm apart.
+EPUCK_MAX_WHEEL_SPEED = 0.1257 # ePuck wheel speed in m/s
+MAX_SPEED = 6.28
+
+# get the time step of the current world.
+SIM_TIMESTEP = int(robot.getBasicTimeStep())
+
+# Initialize Motors
+leftMotor = robot.getDevice('left wheel motor')
+rightMotor = robot.getDevice('right wheel motor')
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
+leftMotor.setVelocity(0.0)
+rightMotor.setVelocity(0.0)
+
+# Initialize and Enable the Ground Sensors
+gsr = [0, 0, 0]
+ground_sensors = [robot.getDevice('gs0'), robot.getDevice('gs1'), robot.getDevice('gs2')]
+for gs in ground_sensors:
+    gs.enable(SIM_TIMESTEP)
+
+# Allow sensors to properly initialize
+for i in range(10): robot.step(SIM_TIMESTEP)  
+
+vL = 0
+vR = 0
+
+# Initialize gps and compass for odometry
+gps = robot.getDevice("gps")
+gps.enable(SIM_TIMESTEP)
+compass = robot.getDevice("compass")
+compass.enable(SIM_TIMESTEP)
+
+# TODO: Find waypoints to navigate around the arena while avoiding obstacles
+waypoints = []
+# Index indicating which waypoint the robot is reaching next
+index = 0
+
+# Get ping pong ball marker that marks the next waypoint the robot is reaching
+marker = robot.getFromDef("marker").getField("translation")
+
+# Main Control Loop:
+while robot.step(SIM_TIMESTEP) != -1:
+    # Set the position of the marker
+    marker.setSFVec3f([waypoints[index][0], waypoints[index][1], 0.01])
+    
+    # Read ground sensor values
+    for i, gs in enumerate(ground_sensors):
+        gsr[i] = gs.getValue()
+
+    # Read pose_x, pose_y, pose_theta from gps and compass
+    pose_x = gps.getValues()[0]
+    pose_y = gps.getValues()[1]
+    pose_theta = np.arctan2(compass.getValues()[0], compass.getValues()[1])
+    
+    # TODO: controller
+    
+    print("Current pose: [%5f, %5f, %5f]" % (xr, yr, theta))
+    leftMotor.setVelocity(vL)
+    rightMotor.setVelocity(vR)
